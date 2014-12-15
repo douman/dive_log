5f1a3488aa08e2ceef5fd2e3a1e8927648c14f59
Doug <drmsys@yahoo.com>
2014-12-14 22:29:53 -0800

    Dive Log Initial Design

---
+++ dive_log.ino
@@ -0,0 +1,41 @@
+// Dive logger software
+// drm 20141214
+// V0.0 Just sketching out the rough outline
+
+void setup() {
+  // Initialize interface
+    // Serial
+    // display
+    // WiFi?
+    // Wireless?
+  // General initalize housekeeping
+    // Print serial number and version info
+  // Initialize sensors
+    // 10dof
+    // depth
+    // gps
+    // RTC
+}
+
+void loop() {
+  // Dispatch commands
+  // d -- data dump
+  // i -- initialize
+  // l -- load dive plan
+  // p -- load parameter database
+  
+  // sensor loop
+  // if interial read 10dof
+  // if depth read depth
+  // if gps read gps
+  // if RTC read RTC
+  
+  // if any logging read RTC
+  // if log write log record
+  // if serial write serial data
+  // if WiFi send data packet
+  // if Wireless send data packet
+  
+  // Calc display values -- Display values
+  // sleep till next event
+}
